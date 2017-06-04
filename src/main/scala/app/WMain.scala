a73f49e9709ac248b8ff6bafc988b517dfd4f929
Ionkin Mikhail <mio@debian8>
2017-06-04 16:35:50 +0300

    Main: remove unused strings

--- src/main/scala/app/WMain.scala
+++ src/main/scala/app/WMain.scala
@@ -5,20 +5,12 @@ import image._
 
 object WMain {
   def main(args: Array[String]) {
-    val resourcesDirectory = new java.io.File("src/main/resources")
-    val dir = resourcesDirectory.getAbsolutePath() + "/"
-    val forVessel = "01_g.jpg" //2//"image_023.jpg" //
-    val forDisk = "1.jpg" //"image_023.jpg" //""
-    val forTiff = "01_g.tif"
-
     def time[R](block: => R) {
       val t0 = System.nanoTime()
       val result = block // call-by-name
       val t1 = System.nanoTime()
       println("Elapsed time: " + (t1 - t0) / 1000000 + "ms")
     }
-    //val res = preprocessing.DilateErose.dilateErose(image.Input.getImage("/home/misha/6-1-5.jpg"))
-    //image.Output.visible(res, "tit")
     time { tests.AnalysTest.vesselSegmentTest }
   }
 }
